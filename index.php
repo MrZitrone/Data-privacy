d4f58b89c218596b3dc5cc00b0dbc20e787fb3d0
MrZitrone <70618349+MrZitrone@users.noreply.github.com>
2024-09-22 12:54:00 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,21 @@
+
+
+<!DOCTYPE html>
+<html>
+    <head>
+    <link rel="stylesheet" href="mystyle.css">
+
+
+    </head>
+
+    <body>
+        <ul>
+            <li><a href="#home">Home</a></li>
+            <li><a href="#news">News</a></li>
+            <li><a href="#contact">Contact</a></li>
+            <li><a href="#about">About</a></li>
+        </ul>
+        <script src="myscript.js"></script>
+
+    </body>
+</html>
